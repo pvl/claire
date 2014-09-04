73d22cc63a2a37bd3c99774bf098ca12c81d54ae
Simeon Franklin <sfranklin@twitter.com>
2014-09-04 08:58:41 -0700

    Add argparse to turn on/off fullscreen behavior

--- funnels.py
+++ funnels.py
@@ -1,19 +1,28 @@
 import pyglet
-
+import argparse
 from levels import GameOver, IntroScreen, TheGame
 from levels.levels import Levels
 
-window = pyglet.window.Window()#fullscreen=True)
+def main(fullscreen):
+  window = pyglet.window.Window(fullscreen=fullscreen)
 
-levels = Levels([IntroScreen(window), TheGame(window), GameOver(window)])
-pyglet.clock.schedule(levels.clock)
+  levels = Levels([IntroScreen(window), TheGame(window), GameOver(window)])
+  pyglet.clock.schedule(levels.clock)
 
-@window.event
-def on_key_press(symbol, modifiers):
-  levels.key(symbol, modifiers)
+  @window.event
+  def on_key_press(symbol, modifiers):
+    levels.key(symbol, modifiers)
 
-@window.event
-def on_draw():
-  levels.draw()
+  @window.event
+  def on_draw():
+    levels.draw()
 
-pyglet.app.run()
+  pyglet.app.run()
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description="Arithemetic practice game.")
+  parser.add_argument('--fullscreen', action="store_true", help='Turn on fullscreen. Defaults to True')
+  parser.add_argument('--no-fullscreen', dest="fullscreen", action="store_false", help='Turn off fullscreen. Defaults to False')
+  parser.set_defaults(fullscreen=True)
+  results = parser.parse_args()
+  main(results.fullscreen)
